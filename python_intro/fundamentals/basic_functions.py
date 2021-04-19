cf45ebe205cc1d8711c5d00d263601b0b62b4f2b
GilbertRPG <yannirocco@gmail.com>
2021-04-19 10:28:08 -0600

    #9 completed

--- python_intro/fundamentals/basic_functions.py
+++ python_intro/fundamentals/basic_functions.py
@@ -47,3 +47,14 @@ def a():
     return 7
 print(a())
 
+#9 returns 7, 14, 21
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))
+
