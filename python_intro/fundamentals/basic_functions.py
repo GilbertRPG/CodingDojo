b27787e4aaac204c69da71706d506587fb550408
GilbertRPG <yannirocco@gmail.com>
2021-04-19 10:24:19 -0600

    # 4, 5, 6, 7 completed

--- python_intro/fundamentals/basic_functions.py
+++ python_intro/fundamentals/basic_functions.py
@@ -13,3 +13,26 @@ def a():
     return 5
     return 10
 print(a())
+
+#4 returns 5
+def a():
+    return 5
+    print(10)
+print(a())
+
+#5 returns null
+def a():
+    print(5)
+x = a()
+print(x)
+
+#6 returns null, null
+# def a(b,c):
+#    print(b+c)
+# print(a(1,2) + a(2,3))
+
+#7 returns 7
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+
