7afe55ae2948c7a9ed37dcfd05f812cd9f9fdc27
GilbertRPG <yannirocco@gmail.com>
2021-04-19 05:21:19 -0600

    Added print from , and + for Noelle

--- python_intro/fundamentals/hello_world.py
+++ python_intro/fundamentals/hello_world.py
@@ -2,4 +2,5 @@
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Noelle"
-print("Hello", name)
+print("Hello" , name)	# with a comma
+print("Hello" + name)	# with a +
