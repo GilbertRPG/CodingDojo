57e79f9c186f0bb2f1caede775bef06a07efbc36
GilbertRPG <yannirocco@gmail.com>
2021-04-19 09:30:07 -0600

    Completed Flexible Counter

--- python_intro/fundamentals/basic_for_loops1.py
+++ python_intro/fundamentals/basic_for_loops1.py
@@ -28,7 +28,7 @@ for x in range(1, 101):
         print(x)
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
-
+print("BigNumber")
 x = 1
 total = 0
 while x < 500000:
@@ -36,3 +36,21 @@ while x < 500000:
     x = x + 2
 print(total)
 
+# Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+print("Count_by_Four")
+x = 2018
+while x >= 0:
+    print(x)
+    x = x - 4
+
+# Flexible Counter (optional) - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. 
+# For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+print("FlexibleCounter")
+lowNum = int(input("LowNumber "))
+highNum = int(input("HighNumber "))
+mult = int(input("Mult "))
+
+while lowNum <= highNum:
+    if lowNum % mult == 0:
+        print(lowNum)
+    lowNum = lowNum + 1
